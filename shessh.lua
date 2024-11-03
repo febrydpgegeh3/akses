d4ee847fe6b572c113477914c8c31153a62d8a08
febrydpgegeh3 <febrydpgegeh3@gmail.com>
2024-11-04 05:25:46 +0700

    Delete shessh.lua

--- shessh.lua
+++
@@ -1,5 +0,0 @@
-local player = game.Players.LocalPlayer
-
-if player.UserId == 7494421921 then -- akses
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/akonber/RBLX/refs/heads/main/elte2m.lua'))("")
-end
