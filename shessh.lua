968ef0fc2ec301a31a82eb04e793204567cf16c7
febrydpgegeh3 <febrydpgegeh3@gmail.com>
2024-11-04 05:24:51 +0700

    Update shessh.lua

--- shessh.lua
+++ shessh.lua
@@ -1,5 +1,5 @@
 local player = game.Players.LocalPlayer
 
-if player.UserId == 176010631 then -- akses
+if player.UserId == 7494421921 then -- akses
     loadstring(game:HttpGet('https://raw.githubusercontent.com/akonber/RBLX/refs/heads/main/elte2m.lua'))("")
 end
