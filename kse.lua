eda0ce4a8013a4e5e23ce59fa11ba9456dbfd4d5
febrydpgegeh3 <febrydpgegeh3@gmail.com>
2024-11-04 07:55:32 +0700

    Update kse.lua

--- kse.lua
+++ kse.lua
@@ -2,17 +2,23 @@ local HttpService = game:GetService("HttpService")
 local player = game.Players.LocalPlayer
 local Players = game:GetService("Players")
 
--- Daftar ID pemain yang harus dikeluarkan
-local adminIDs = {
-    [202038212] = true,  -- Ganti dengan ID pemain yang ingin dikeluarkan
-    [987654321] = true,  -- Ganti dengan ID pemain yang ingin dikeluarkan
-}
-
 -- Data akses lokal (UserId, Key)
 local accessData = {
     [7523995740] = { key = "key1" }, -- ID Pemain dan Key
     [176010632] = { key = "key2" }, -- ID Pemain dan Key
     -- Tambahkan UserId dan key lainnya sesuai kebutuhan
+}
+
+-- Daftar ID pemain yang ingin diberi notifikasi saat bergabung
+local notifyUserIds = {
+    [202038212] = true,  -- Ganti dengan ID pemain yang ingin diberi notifikasi
+    [127711155] = true,  -- Ganti dengan ID pemain yang ingin diberi notifikasi
+    [695146286] = true,  -- Ganti dengan ID pemain yang ingin diberi notifikasi
+    [489401796] = true,  -- Ganti dengan ID pemain yang ingin diberi notifikasi
+    [138509994] = true,  -- Ganti dengan ID pemain yang ingin diberi notifikasi
+    [71243418] = true,  -- Ganti dengan ID pemain yang ingin diberi notifikasi
+    [44038206] = true,  -- Ganti dengan ID pemain yang ingin diberi notifikasi
+    [2828604791] = true,  -- Ganti dengan ID pemain yang ingin diberi notifikasi
 }
 
 -- Fungsi untuk menampilkan notifikasi
@@ -82,25 +88,21 @@ button.MouseButton1Click:Connect(function()
     end
 end)
 
--- Fungsi untuk memeriksa pemain yang sudah ada di dalam game
+-- Fungsi untuk memeriksa pemain yang sudah dalam permainan
 local function checkExistingPlayers()
     for _, existingPlayer in pairs(Players:GetPlayers()) do
-        if adminIDs[existingPlayer.UserId] then
-            notify("Admin Bergabung!", existingPlayer.Name .. " sudah ada di dalam server.", 5)
-            wait(1) -- Sedikit delay sebelum kick
-            existingPlayer:Kick("Anda tidak diizinkan untuk bergabung.")
+        if notifyUserIds[existingPlayer.UserId] then
+            notify("WARNING!", existingPlayer.Name .. " ADMIN IN THIS SERVER.", 5)
         end
     end
 end
 
--- Memeriksa pemain yang bergabung
-Players.PlayerAdded:Connect(function(newPlayer)
-    if adminIDs[newPlayer.UserId] then
-        notify("Admin Bergabung!", newPlayer.Name .. " telah bergabung ke dalam server.", 5)
-        wait(1) -- Sedikit delay sebelum kick
-        newPlayer:Kick("Anda tidak diizinkan untuk bergabung.")
-    end
-end)
-
 -- Panggil fungsi untuk memeriksa pemain yang sudah ada saat skrip dijalankan
 checkExistingPlayers()
+
+-- Fungsi untuk memeriksa pemain yang bergabung
+Players.PlayerAdded:Connect(function(newPlayer)
+    if notifyUserIds[newPlayer.UserId] then
+        notify("WARNING!", newPlayer.Name .. " ADMIN JOIN.", 5)
+    end
+end)
